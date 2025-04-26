022a358ea0e0bd035abb311c6b5f3475bcdcd32d
deryhlazov <Serhii.Deryhlazov@quest.com>
2025-04-26 15:04:20 +0200

    fix

--- Controllers/ListingsController.cs
+++ Controllers/ListingsController.cs
@@ -32,8 +32,8 @@ public class ListingsController : ControllerBase
 
         if (!string.IsNullOrWhiteSpace(request.SortBy))
         {
-            query = request.OrderBy == "desc" 
-                ? query.OrderByDescending(l => EF.Property<object>(l, request.SortBy)) 
+            query = request.OrderBy == "desc"
+                ? query.OrderByDescending(l => EF.Property<object>(l, request.SortBy))
                 : query.OrderBy(l => EF.Property<object>(l, request.SortBy));
         }
 
@@ -55,44 +55,6 @@ public class ListingsController : ControllerBase
                 Picture1 = l.Picture1
             })
             .ToListAsync();
-
-        return Ok(new GetAllListingsResponse
-        {
-            TotalNumber = totalNumber,
-            Listings = listings,
-            Page = request.Page
-        });
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<GetAllListingsResponse>> GetAllListings([FromQuery] GetAllListingsRequest request)
-    {
-        var query = _db.Listings.AsQueryable();
-
-        if (!string.IsNullOrWhiteSpace(request.Search))
-        {
-            query = query.Where(l => l.Title.Contains(request.Search) || l.Description.Contains(request.Search));
-        }
-
-        if (!string.IsNullOrWhiteSpace(request.Category))
-        {
-            query = query.Where(l => l.Category == request.Category);
-        }
-
-        if (!string.IsNullOrWhiteSpace(request.SortBy))
-        {
-            if (request.OrderBy == "desc")
-            {
-                query = query.OrderByDescending(l => EF.Property<object>(l, request.SortBy));
-            }
-            else
-            {
-                query = query.OrderBy(l => EF.Property<object>(l, request.SortBy));
-            }
-        }
-
-        var totalNumber = await query.CountAsync();
-        var listings = await query.Skip((request.Page - 1) * request.Limit).Take(request.Limit).ToListAsync();
 
         return Ok(new GetAllListingsResponse
         {
--- Models/Responses/GetAllListingsResponse.cs
+++ Models/Responses/GetAllListingsResponse.cs
@@ -3,6 +3,6 @@ namespace MaltalistApi.Models;
 public class GetAllListingsResponse
 {
     public int TotalNumber { get; set; }
-    public List<Listing> Listings { get; set; }
+    public List<ListingSummaryResponse> Listings { get; set; }
     public int Page { get; set; }
 }
